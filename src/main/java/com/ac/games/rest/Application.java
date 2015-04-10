646fefd126d4ba8348fa3b7ec45a546c881a7c8b
apshaiTerp <apshaiTerp@yahoo.com>
2015-04-09 19:38:02 -0500

    Updating rest files

--- src/main/java/com/ac/games/rest/Application.java
+++ src/main/java/com/ac/games/rest/Application.java
@@ -24,7 +24,8 @@ public class Application extends SpringBootServletInitializer {
   public static GamesDatabase database;
   
   /** The Host URL for our Mongo Instance */
-  public static String databaseHost = "192.168.1.9";
+  //TODO - public static String databaseHost = "192.168.1.9";
+  public static String databaseHost = "localhost";
   /** The Host Port for our Mongo Instance */
   public static int    databasePort = 27017;
   /** The Host Database Name for our Mongo Database */
