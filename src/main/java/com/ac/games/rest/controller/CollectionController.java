d03ecf9e0d0a7d32f208ae8fdee83a0346178948
apshaiTerp <apshaiTerp@yahoo.com>
2015-04-27 21:30:41 -0500

    Updating CollectionController to support new GET options

--- src/main/java/com/ac/games/rest/controller/CollectionController.java
+++ src/main/java/com/ac/games/rest/controller/CollectionController.java
@@ -1,5 +1,9 @@
 package com.ac.games.rest.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ac.games.data.Collection;
 import com.ac.games.data.CollectionItem;
+import com.ac.games.data.CompactSearchData;
+import com.ac.games.data.GameType;
 import com.ac.games.db.GamesDatabase;
 import com.ac.games.db.MongoDBFactory;
 import com.ac.games.db.exception.ConfigurationException;
@@ -32,6 +38,7 @@ import com.ac.games.rest.message.SimpleMessageData;
 @RequestMapping("/collection")
 public class CollectionController {
 
+  private final static SimpleDateFormat formatter = new SimpleDateFormat("MMM d, yyyy");
   /**
    * GET method designed to handle retrieving {@link Collection} data from the database.
    * This method supports the following parameters:
@@ -45,17 +52,105 @@ public class CollectionController {
    * @return A {@link Collection} object or {@link SimpleErrorData} message reporting what failed.
    */
   @RequestMapping(method = RequestMethod.GET, produces="application/json;charset=UTF-8")
-  public Object getCollection(@RequestParam(value="collectionid") long collectionID) {
+  public Object getCollection(@RequestParam(value="collectionid") long collectionID,
+                              @RequestParam(value="topx", defaultValue = "-1") int topX,
+                              @RequestParam(value="compact", defaultValue = "no") String compact) {
     if (collectionID <= 0)
       return new SimpleErrorData("Collection Data Error", "There was no valid collection request data provided");
+    
+    if ((!compact.equalsIgnoreCase("no")) && (!compact.equalsIgnoreCase("yes")) && (!compact.equalsIgnoreCase("n")) && (!compact.equalsIgnoreCase("y")))
+      return new SimpleErrorData("Collection Data Error", "The compact value " + compact + " is not an accepted value.");
       
     GamesDatabase database = null; 
-    Collection collection  = null;
+    Object results         = null;
     try {
       database = MongoDBFactory.createMongoGamesDatabase(Application.databaseHost, Application.databasePort, Application.databaseName);
       database.initializeDBConnection();
       
-      collection = database.readCollection(collectionID);
+      if ((topX == -1) && ((compact.equalsIgnoreCase("no") || compact.equalsIgnoreCase("n"))))
+        results = database.readCollection(collectionID);
+      else if (topX > 0) {
+        Collection collection = database.readCollection(collectionID);
+        if (collection == null)
+          results = null;
+        else {
+          List<CollectionItem> allItems = collection.getGames();
+          if ((allItems == null) || (allItems.size() == 0))
+            results = new ArrayList<CompactSearchData>();
+          else {
+            //Now we need to sort and choose
+            Collections.sort(allItems);
+            List<CompactSearchData> resultList = new ArrayList<CompactSearchData>();
+            for (int i = 0; (i < topX) && (i < allItems.size()); i++) {
+              CollectionItem item = allItems.get(i);
+              CompactSearchData data = new CompactSearchData();
+              String displayString = item.getGame().getName();
+              if (item.getGame().getYearPublished() != -1)
+                displayString += " (" + item.getGame().getYearPublished() + ")";
+              data.setDisplayString(displayString);
+              data.setSourceID(item.getItemID());
+              data.setThumbnailURL(item.getGame().getImageThumbnailURL());
+              //Overload the sourceField value to put displayable text
+              if (item.getDateAcquired() != null)
+                data.setSourceField("Game acquired on: " + formatter.format(item.getDateAcquired()));
+              else data.setSourceField("Game ID: " + item.getGameID());
+              resultList.add(data);
+            }
+          }
+        }
+      } else if (compact.equalsIgnoreCase("yes") || compact.equalsIgnoreCase("y")) {
+        Collection collection = database.readCollection(collectionID);
+        if (collection == null)
+          results = null;
+        else {
+          List<CollectionItem> allItems = collection.getGames();
+          if ((allItems == null) || (allItems.size() == 0))
+            results = new ArrayList<CollectionItem>();
+          else {
+            List<CollectionItem> resultList = new LinkedList<CollectionItem>();
+            
+            //First Loop, pick out all Base and Collectible games
+            for (CollectionItem item : allItems) {
+              if ((item.getGame().getGameType() == GameType.BASE) || (item.getGame().getGameType() == GameType.COLLECTIBLE))
+                resultList.add(item);
+            }
+            
+            //Second Loop, pick out all Expansions, and link them to their parent game if possible.
+            for (CollectionItem item : allItems) {
+              if (item.getGame().getGameType() == GameType.EXPANSION) {
+                boolean found = false;
+                for (CollectionItem parentItem : resultList) {
+                  if (parentItem.getGameID() == item.getGame().getParentGameID()) {
+                    CompactSearchData data = new CompactSearchData();
+                    String displayString = item.getGame().getName();
+                    if (item.getGame().getYearPublished() != -1)
+                      displayString += " (" + item.getGame().getYearPublished() + ")";
+                    data.setDisplayString(displayString);
+                    data.setSourceID(item.getItemID());
+                    data.setThumbnailURL(item.getGame().getImageThumbnailURL());
+                    data.setSourceField("Game ID: " + item.getGameID());
+                    
+                    List<CompactSearchData> expansions = parentItem.getExpansionList();
+                    if (expansions == null) expansions = new ArrayList<CompactSearchData>();
+                    expansions.add(data);
+                    
+                    parentItem.setExpansionList(expansions);
+                    found = true;
+                    break;
+                  }
+                }
+                if (!found) {
+                  //If we somehow have an expansion with no parent, then just add it to the list
+                  resultList.add(item);
+                }
+              }
+            }//end for second loop
+            
+            Collections.sort(resultList);
+            results = resultList;
+          }
+        }
+      }
     } catch (DatabaseOperationException doe) {
       doe.printStackTrace();
       try { if (database != null) database.closeDBConnection(); } catch (Throwable t2) { /** Ignore Errors */ }
@@ -68,9 +163,9 @@ public class CollectionController {
       try { if (database != null) database.closeDBConnection(); } catch (Throwable t2) { /** Ignore Errors */ }
     }
     
-    if (collection == null)
+    if (results == null)
       return new SimpleErrorData("Collection Not Found", "The requested collection could not be found in the database.");
-    return collection;
+    return results;
   }
   
   /**
